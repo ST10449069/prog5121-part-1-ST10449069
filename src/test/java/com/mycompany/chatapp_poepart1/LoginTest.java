65e5c7215123c4dee7032ff8b435a75f1cb680c8
ST10449069 <161335940+ST10449069@users.noreply.github.com>
2026-04-13 19:04:24 +0200

    fourth commit

---
+++ src/test/java/com/mycompany/chatapp_poepart1/LoginTest.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.chatapp_poepart1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    @Test
+    public void testRegisterUser() {
+    }
+
+    @Test
+    public void testCheckUserName() {
+    }
+
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+    @Test
+    public void testCheckCellPhoneNumber() {
+    }
+
+    @Test
+    public void testLoginUser() {
+    }
+
+    @Test
+    public void testReturnLoginStatus() {
+    }
+    
+}
